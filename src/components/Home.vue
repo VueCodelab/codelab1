7b92523e54e9b1609464c3c95d860359b11831e6
Felix Barros <soyfelixbarros@gmail.com>
2018-03-09 22:00:17 -0300

    revert

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,100 @@
 <template>
   <div class="home">
-    <img src="https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/980879/580/385/m1/fpnw/wm0/start!12-01-01-.jpg?1455179257&s=df9501de82074ffe2a7643b7fd1f2040">
-    <h1>Home</h1>
+    <h1>CodeLab v2 - Jugando con Vue.js</h1>
+    <h2>9 de Marzo de 2018</h2>
+    <h2>En la ciudad de Corrientes - Argentina</h2>
+    <br>
+    <h2>Participantes</h2>
+    <br>
+    <h4>
+       <ul>
+         <li><a href="https://github.com/learod">Leandro</a></li>
+         <li><a href="https://github.com/leferreyra"></a>Leonardo</li>
+         <li><a href="https://github.com/matiasmasca"></a>Matías</li>
+         <li><a href="https://github.com/ricardogauto11"></a>Ricardo</li>
+         <li><a href="https://github.com/the-guish"></a>Luis</li>
+         <li><a href="https://github.com/gabrielc63"></a>Gabriel</li>
+         <li><a href="https://github.com/mrgj"></a>Gustavo</li>
+         <li><a href="https://github.com/soyFelixBarros"></a>Felix</li>
+         <li><a href="https://github.com/Briant-Moreira"></a>Brian</li>
+       </ul>
+      Brain, Matias, Gabriel, Leandro, Felix, Roberto, Gustavo, Leonardo, Luis
+    </h4>
+    <br>
+    <ul>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          target="_blank"
+        >
+          Reemplace por el nombre y enlace a su componente
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/components/Home'
-<<<<<<< HEAD
-import Form from '@/components/Form'
-=======
 import List from '@/components/List'
->>>>>>> 670a93425c8d2ff24ded56470d75c09bf465ea96
 
 Vue.use(Router)
 
@@ -18,15 +14,9 @@ export default new Router({
       component: Home
     },
     {
-<<<<<<< HEAD
-      path: '/form',
-      name: 'Form',
-      component: Form
-=======
       path: '/list',
       name: 'List',
       component: List
->>>>>>> 670a93425c8d2ff24ded56470d75c09bf465ea96
     }
   ]
 })
